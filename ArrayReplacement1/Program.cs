0c69b2afacccdd62b066bd97d83a1d0599e4fd58
7even <7even@7EVEN->
2020-06-11 16:21:34 +0600

    Добавьте файлы проекта.

---
+++ ArrayReplacement1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArrayReplacement1
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] array = new int[10];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i] <= 0)
+                    Console.WriteLine("X[" + i + "] = " + 1);
+                else
+                    Console.WriteLine("X[" + i + "] = " + array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
